17adbb5ebb604d576b1ace3f78951d84d0ec9a10
Tomilade18 <jboyyumzey@gmail.com>
2024-06-13 19:03:59 +0100

    setting layout

--- multi-step-form/src/ui/AddOns.jsx
+++ multi-step-form/src/ui/AddOns.jsx
@@ -1,5 +1,15 @@
 export default function AddOn () {
     return(
-        <div></div>
+        <div>
+             <form action="">
+                <input type="checkbox" />
+                <label htmlFor="">Lorem,</label>
+                <input type="checkbox" />
+                <label htmlFor="">Lorem,</label>
+                <input type="checkbox" />
+                <label htmlFor="">Lorem,</label>
+                <button>Submit</button>
+            </form>
+        </div>
     )
 }
--- multi-step-form/src/ui/PersonalInfo.jsx
+++ multi-step-form/src/ui/PersonalInfo.jsx
@@ -1,5 +1,7 @@
 export default function PersonalInfo () {
     return(
-        <div></div>
+        <div>
+           
+        </div>
     )
 }
